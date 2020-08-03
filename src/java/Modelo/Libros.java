41b45d7354e4a00b8b942283f43127a288d59b8b
Carlos Carbajal <carloscarbajalrojas@hotmail.com>
2020-08-03 12:29:53 -0500

    Aplicacion Biblioteca: Generando

--- src/java/Modelo/Libros.java
+++ src/java/Modelo/Libros.java
@@ -10,5 +10,80 @@ package Modelo;
  * @author TuNombre
  */
 public class Libros {
+    private int idlibro;
+    private String nombre;
+    private String autor;
+    private String año;
+    private String estado;
+
+    /**
+     * @return the idlibro
+     */
+    public int getIdlibro() {
+        return idlibro;
+    }
+
+    /**
+     * @param idlibro the idlibro to set
+     */
+    public void setIdlibro(int idlibro) {
+        this.idlibro = idlibro;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the autor
+     */
+    public String getAutor() {
+        return autor;
+    }
+
+    /**
+     * @param autor the autor to set
+     */
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    /**
+     * @return the año
+     */
+    public String getAño() {
+        return año;
+    }
+
+    /**
+     * @param año the año to set
+     */
+    public void setAño(String año) {
+        this.año = año;
+    }
+
+    /**
+     * @return the estado
+     */
+    public String getEstado() {
+        return estado;
+    }
+
+    /**
+     * @param estado the estado to set
+     */
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
     
 }
